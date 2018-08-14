1d26d22363a712561bdf3c27d4cce01edf0c153f
MI <>
2018-08-14 17:55:31 +0530

    sort and display elements in an array

---
+++ InterviewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InterviewDemo
+//
+//  Created by MI on 14/08/18.
+//  Copyright © 2018 MAGNAHEALTHSOLUTIONS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var array = [120,60,30,150,200,300,500,25,32,7]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        array = array.sorted(by: <)
+        print(array)
+        print(array.prefix(5))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
