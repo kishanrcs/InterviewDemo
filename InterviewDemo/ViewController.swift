46bbf51814d4b29afcab007935c5f7102bc936d5
kishanrcs <kishan.ramisetty@gmail.com>
2018-08-16 17:12:13 +0530

    Added fibonacci series functionality..

--- InterviewDemo/ViewController.swift
+++ InterviewDemo/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 var array = [120,60,30,150,200,300,500,25,32,7]
+    let fibArray = NSMutableArray()
     
     @IBOutlet weak var lbl_display: UILabel!
     override func viewDidLoad() {
@@ -19,6 +20,20 @@ var array = [120,60,30,150,200,300,500,25,32,7]
         print(array.prefix(5))
         print(array.count)
         lbl_display.text = "printed first 5 small elements"
+        fibonacci(n: 7)
+    }
+    
+    func fibonacci(n : Int)  {
+        var num1 = 0
+        var num2 = 1
+        for _ in 0..<n{
+            let num = num1 + num2
+            num1 = num2
+            num2 = num
+            print("result = \(num2)")
+            fibArray.add(num2)
+        }
+        print(fibArray)
     }
 
     override func didReceiveMemoryWarning() {
